5b0b27aa6f9dc17e26c8f8437ae2654f84d1bc14
aamirnaqvi200-cpu <aamirnaqvi200@gmail.com>
2025-08-20 15:40:22 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -530,7 +530,7 @@ function App() {
   </h3>
   <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 max-w-5xl mx-auto">
     {[
-      "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+      "https://ia801002.us.archive.org/18/items/shorts-2-part/inkwell%20carzyyyy2.mp4",
       "https://ia600902.us.archive.org/33/items/part-1-shorts/Inklwell%20media%20reel%201%20v3.mp4",
       "https://ia801002.us.archive.org/18/items/shorts-2-part/Mj%20real_2.mp4",
       "https://ia801007.us.archive.org/2/items/inkwell-media-video-1-v-2/inkwell%20media%20video%201%20v2.mp4",
