010d04be5845475a2285a97888b1454b1ed945d6
aamirnaqvi200-cpu <aamirnaqvi200@gmail.com>
2025-08-24 01:28:33 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -173,7 +173,7 @@ function App() {
         scaleY: 3,
         scrollTrigger: {
           trigger: heroRef.current,
-          start: "bottom bottom",
+          start: "top bottom",
           end: "top top",
           scrub: 1,
         }
